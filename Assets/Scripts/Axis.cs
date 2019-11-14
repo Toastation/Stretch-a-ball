eb316fbfb45425dad0896a57bee4c618cb598315
Victor <victor.chapal34@gmail.com>
2019-11-14 14:50:34 +0100

    test axes

---
+++ Assets/Scripts/Axis.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Axis : MonoBehaviour
+{
+    public GameObject objetOrigin;
+
+    void OnDrawGizmos()
+    {
+        Vector3 origin = new Vector3();
+        origin = objetOrigin.transform.position;
+        Vector3 origin2 = new Vector3(0, 0, 0);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(origin2, Vector3.right);
+        Gizmos.DrawLine(origin2, Vector3.up);
+        Gizmos.DrawLine(origin2, Vector3.forward);
+    }
+
+}
