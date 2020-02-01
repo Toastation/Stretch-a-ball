b02c8da710ca9a84f77438f302c4050749b56946
Lucas T <lucas.trocherie72@gmail.com>
2020-01-31 17:16:28 -0800

    New Script for Menu Interaction : It Works !

---
+++ Assets/Scripts/MenuInteraction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity.Interaction;
+
+public class MenuInteraction : MonoBehaviour
+{
+    CurrentMenu cMenu;
+    InteractionButton Button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cMenu = GameObject.Find("Palm UI").GetComponent<CurrentMenu>();
+        Button = GameObject.Find("Button Creation").GetComponent<InteractionButton>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Button.isPressed)
+            cMenu.SetMenu(CurrentMenu.Menu.Creation);
+    }
+}
