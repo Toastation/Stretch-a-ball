c08cdc606e0c22ecaa3695747bfc66b69f4d8616
Lucas T <lucas.trocherie72@gmail.com>
2020-02-03 01:51:10 -0800

    Added the sub menu for selection : It works !

--- Assets/Scripts/CurrentMenu.cs
+++ Assets/Scripts/CurrentMenu.cs
@@ -16,11 +16,35 @@ public class CurrentMenu : MonoBehaviour
         Quit
     }
 
+    //enumerating the different selections
+    public enum Selection
+    {
+        NoSelection,
+        Modification,
+        Erase,
+        SetOperation,
+        Return
+    }
+
+    //enumerating the different Set Operations
+    public enum SetOperation
+    {
+        NoOperation,
+        SetIntersection,
+        SetUnion,
+        SetRelativeComplement,
+        Return
+    }
+
     TextMesh textObject; // textObject is the text displayed on the menu screen
+
+    // Stocking the different menus
     private Menu currentMenu; // Menu is private for oriented object programmming purposes
+    private Selection currentSelection;
+    private SetOperation currentSetOperation;
 
     // GetCurrentMenu returns the current menu
-    Menu GetCurrentMenu()
+    public Menu GetCurrentMenu()
     {
         return currentMenu;
     }
@@ -54,21 +78,90 @@ public class CurrentMenu : MonoBehaviour
                 textObject.text = "Quit";
                 break;
             default:
-                textObject.text = "Error    ";
+                textObject.text = "Error in Current Menu";
                 break;
-                return 0;
         }
         return 0;
     }
+
+    // SetSelection changes the value of the current selection
+    public int SetSelection(Selection newSelection)
+    {
+        currentSelection = newSelection;
+        // Communicating the new menu to the 3D text
+        if(currentMenu == Menu.Selection)
+        {
+            switch (currentSelection)
+            {
+                case Selection.NoSelection:
+                    textObject.text = "Selection - No Selection in current mode";
+                    break;
+                case Selection.Modification:
+                    textObject.text = "Selection - Modification";
+                    break;
+                case Selection.Erase:
+                    textObject.text = "Selection - Erase";
+                    break;
+                case Selection.SetOperation:
+                    textObject.text = "Selection - Set Operation";
+                    break;
+                case Selection.Return:
+                    textObject.text = "Selection - Return";
+                    break;
+                default:
+                    textObject.text = "Error in Current Selection";
+                    break;
+            }
+        }
+        
+        return 0;
+    }
+
+    // SetSelection changes the value of the current set operation
+    public int SetSetOperation(SetOperation newSetOperation)
+    {
+        currentSetOperation = newSetOperation;
+        // Communicating the new menu to the 3D text
+        if (currentSelection == Selection.SetOperation)
+        {
+            switch (currentSetOperation)
+            {
+                case SetOperation.NoOperation:
+                    textObject.text = "Set Operation - No operation in current mode";
+                    break;
+                case SetOperation.SetIntersection:
+                    textObject.text = "Set Operation - Intersection";
+                    break;
+                case SetOperation.SetUnion:
+                    textObject.text = "Set Operation - Union";
+                    break;
+                case SetOperation.SetRelativeComplement:
+                    textObject.text = "Set Operation - Relative Complement";
+                    break;
+                case SetOperation.Return:
+                    textObject.text = "Set Operation - Return";
+                    break;
+                default:
+                    textObject.text = "Error in Current Selection";
+                    break;
+            }
+        }
+
+        return 0;
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
         //Finds the display screen
         textObject = GameObject.Find("MenuName").GetComponent<TextMesh>();
-        // Initiates the menu value 
-        currentMenu = Menu.NoMenuSelected;
-        textObject.text = "No Menu Selected";
+
+        // Initiates the menu values 
+        SetMenu(Menu.NoMenuSelected);
+        SetSelection(Selection.NoSelection);
+        SetSetOperation(SetOperation.NoOperation);
+
         
     }
 
--- Assets/Scripts/MenuInteraction.cs
+++ Assets/Scripts/MenuInteraction.cs
@@ -14,17 +14,72 @@ public class MenuInteraction : MonoBehaviour
     InteractionButton ButtonQuit;
 
 
+    void MenuManager()
+    {
+        // Checks whether a button is pressed
+
+        switch (cMenu.GetCurrentMenu())
+        {
+            case CurrentMenu.Menu.Selection:
+                {
+                    if (ButtonCreation.isPressed)
+                        cMenu.SetSelection(CurrentMenu.Selection.Modification);
+
+                    if (ButtonSelection.isPressed)
+                        cMenu.SetSelection(CurrentMenu.Selection.Erase);
+
+                    if (ButtonStatistics.isPressed)
+                        cMenu.SetSelection(CurrentMenu.Selection.SetOperation);
+
+                    if (ButtonHide_Show.isPressed)
+                    {
+                        cMenu.SetSelection(CurrentMenu.Selection.Return);
+                        cMenu.SetMenu(CurrentMenu.Menu.NoMenuSelected);
+                        cMenu.SetSelection(CurrentMenu.Selection.NoSelection);
+                    }
+
+                    break;
+                }
+
+            default: // General Menu Behavior
+                {
+                    if (ButtonCreation.isPressed)
+                        cMenu.SetMenu(CurrentMenu.Menu.Creation);
+
+                    if (ButtonSelection.isPressed)
+                        cMenu.SetMenu(CurrentMenu.Menu.Selection);
+
+                    if (ButtonStatistics.isPressed)
+                        cMenu.SetMenu(CurrentMenu.Menu.Statistics);
+
+                    if (ButtonHide_Show.isPressed)
+                        cMenu.SetMenu(CurrentMenu.Menu.Hide_Show);
+
+                    if (ButtonHelp_Options.isPressed)
+                        cMenu.SetMenu(CurrentMenu.Menu.Help_Options);
+
+                    if (ButtonQuit.isPressed)
+                        cMenu.SetMenu(CurrentMenu.Menu.Quit);
+                    break;
+                }
+                
+        }
+
+        return;
+       
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         //Initialisation of the different menu buttons
         cMenu = GameObject.Find("Palm UI").GetComponent<CurrentMenu>();
-        ButtonCreation = GameObject.Find("Button Creation").GetComponent<InteractionButton>();
-        ButtonSelection = GameObject.Find("Button Selection").GetComponent<InteractionButton>();
-        ButtonStatistics = GameObject.Find("Button Statistics").GetComponent<InteractionButton>();
-        ButtonHide_Show = GameObject.Find("Button Hide/Show").GetComponent<InteractionButton>();
-        ButtonHelp_Options = GameObject.Find("Button Help/Options").GetComponent<InteractionButton>();
-        ButtonQuit = GameObject.Find("Button Quit").GetComponent<InteractionButton>();
+        ButtonCreation = GameObject.Find("Button Creation").GetComponent<InteractionButton>(); // Button for Creation/Modification/Intersection
+        ButtonSelection = GameObject.Find("Button Selection").GetComponent<InteractionButton>(); // Button for Selection/Erase/Union
+        ButtonStatistics = GameObject.Find("Button Statistics").GetComponent<InteractionButton>(); // Button for Statsistics/Operation/Relative Complement
+        ButtonHide_Show = GameObject.Find("Button Hide/Show").GetComponent<InteractionButton>(); // Button for Hide_Show/Return/Return
+        ButtonHelp_Options = GameObject.Find("Button Help/Options").GetComponent<InteractionButton>(); // Button for Help_Options
+        ButtonQuit = GameObject.Find("Button Quit").GetComponent<InteractionButton>(); // Button for Quit
 
 
     }
@@ -32,24 +87,6 @@ public class MenuInteraction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Checks whether a button is pressed
-
-        if (ButtonCreation.isPressed)
-            cMenu.SetMenu(CurrentMenu.Menu.Creation);
-
-        if (ButtonSelection.isPressed)
-            cMenu.SetMenu(CurrentMenu.Menu.Selection);
-
-        if (ButtonStatistics.isPressed)
-            cMenu.SetMenu(CurrentMenu.Menu.Statistics);
-
-        if (ButtonHide_Show.isPressed)
-            cMenu.SetMenu(CurrentMenu.Menu.Hide_Show);
-
-        if (ButtonHelp_Options.isPressed)
-            cMenu.SetMenu(CurrentMenu.Menu.Help_Options);
-
-        if (ButtonQuit.isPressed)
-            cMenu.SetMenu(CurrentMenu.Menu.Quit);
+        MenuManager();
     }
 }
