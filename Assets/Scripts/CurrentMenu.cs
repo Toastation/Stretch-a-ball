e6cff684054b7638547e1e99d71c6c1be27ff97d
Lucas T <lucas.trocherie72@gmail.com>
2020-02-06 17:20:38 -0800

    The preview in the menu works great !

--- Assets/Scripts/CurrentMenu.cs
+++ Assets/Scripts/CurrentMenu.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CurrentMenu : MonoBehaviour
 {
+
+    #region Pages
+
     //enumerating the different Menus
     public enum Menu
     {
@@ -36,12 +39,27 @@ public class CurrentMenu : MonoBehaviour
         Return
     }
 
+    #endregion
+
+    #region Objects
+
     TextMesh textObject; // textObject is the text displayed on the menu screen
 
     // Stocking the different menus
     private Menu currentMenu; // Menu is private for oriented object programmming purposes
     private Selection currentSelection;
     private SetOperation currentSetOperation;
+
+    //GetCurrentMenuSetOperation returns the current SetOperation
+
+    #endregion
+
+    #region Getters
+
+    public SetOperation GetCurrentMenuSetOperation()
+    {
+        return currentSetOperation;
+    }
 
     // GetCurrentMenuSelection returns the current menu
     public Selection GetCurrentMenuSelection()
@@ -55,12 +73,49 @@ public class CurrentMenu : MonoBehaviour
         return currentMenu;
     }
 
+    #endregion
+
+    #region Set and Display
     // SetMenu changes the value of the current menu
     public int SetMenu(Menu newMenu)
     {
         currentMenu = newMenu;
         // Communicating the new menu to the 3D text
         switch (currentMenu)
+        {
+            case Menu.NoMenuSelected:
+                textObject.text = "No Menu Selected";
+                break;
+            case Menu.Creation:
+                textObject.text = "Creation";
+                break;
+            case Menu.Selection:
+                textObject.text = "Selection";
+                break;
+            case Menu.Statistics:
+                textObject.text = "Statistics";
+                break;
+            case Menu.Hide_Show:
+                textObject.text = "Hide/Show";
+                break;
+            case Menu.Help_Options:
+                textObject.text = "Help/Options";
+                break;
+            case Menu.Quit:
+                textObject.text = "Quit";
+                break;
+            default:
+                textObject.text = "Error in Current Menu";
+                break;
+        }
+        return 0;
+    }
+
+    // DisplayMenu changes the value of the current menu
+    public int DisplayMenu(Menu newMenu)
+    {
+        // Communicating the new menu to the 3D text
+        switch (newMenu)
         {
             case Menu.NoMenuSelected:
                 textObject.text = "No Menu Selected";
@@ -123,6 +178,38 @@ public class CurrentMenu : MonoBehaviour
         return 0;
     }
 
+    // DisplaySelection changes the value of the current selection
+    public int DisplaySelection(Selection newSelection)
+    {
+        // Communicating the menu to the 3D text
+        if (currentMenu == Menu.Selection)
+        {
+            switch (newSelection)
+            {
+                case Selection.NoSelection:
+                    textObject.text = "Selection - No Selection in current mode";
+                    break;
+                case Selection.Modification:
+                    textObject.text = "Selection - Modification";
+                    break;
+                case Selection.Erase:
+                    textObject.text = "Selection - Erase";
+                    break;
+                case Selection.SetOperation:
+                    textObject.text = "Selection - Set Operation";
+                    break;
+                case Selection.Return:
+                    textObject.text = "Selection - Return";
+                    break;
+                default:
+                    textObject.text = "Error in Current Selection";
+                    break;
+            }
+        }
+
+        return 0;
+    }
+
     // SetSelection changes the value of the current set operation
     public int SetSetOperation(SetOperation newSetOperation)
     {
@@ -156,6 +243,41 @@ public class CurrentMenu : MonoBehaviour
         return 0;
     }
 
+    // SetSelection changes the value of the current set operation
+    public int DisplaySetOperation(SetOperation newSetOperation)
+    {
+        // Communicating the new menu to the 3D text
+        if (currentSelection == Selection.SetOperation)
+        {
+            switch (newSetOperation)
+            {
+                case SetOperation.NoOperation:
+                    textObject.text = "Set Operation - No operation in current mode";
+                    break;
+                case SetOperation.SetIntersection:
+                    textObject.text = "Set Operation - Intersection";
+                    break;
+                case SetOperation.SetUnion:
+                    textObject.text = "Set Operation - Union";
+                    break;
+                case SetOperation.SetRelativeComplement:
+                    textObject.text = "Set Operation - Relative Complement";
+                    break;
+                case SetOperation.Return:
+                    textObject.text = "Set Operation - Return";
+                    break;
+                default:
+                    textObject.text = "Error in Current Selection";
+                    break;
+            }
+        }
+
+        return 0;
+    }
+
+    #endregion
+
+    #region Unity Methods
 
     // Start is called before the first frame update
     void Start()
@@ -183,4 +305,5 @@ public class CurrentMenu : MonoBehaviour
 
         
     }
+    #endregion
 }
--- Assets/Scripts/MenuInteraction.cs
+++ Assets/Scripts/MenuInteraction.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Leap.Unity.Interaction;
 using System.Timers;
 using System.Diagnostics;
+using System;
 
 public class MenuInteraction : MonoBehaviour
 {
@@ -117,6 +118,131 @@ public class MenuInteraction : MonoBehaviour
        
     }
 
+    private void MenuPreview() // MenuPreview gives a preview of the button's action
+    {
+        // Checks whether a button is hovered
+
+        switch (cMenu.GetCurrentMenu())
+        {
+            
+            case CurrentMenu.Menu.Selection:
+                {
+                    switch (cMenu.GetCurrentMenuSelection())
+                    {
+                        case CurrentMenu.Selection.SetOperation: // Set Operation Menu behavior
+                            {
+                                if (ButtonCreation.isPrimaryHovered)
+                                {
+                                    cMenu.DisplaySetOperation(CurrentMenu.SetOperation.SetIntersection);
+                                    break;
+                                }
+
+                                if (ButtonSelection.isPrimaryHovered)
+                                {
+                                    cMenu.DisplaySetOperation(CurrentMenu.SetOperation.SetUnion);
+                                    break;
+                                }
+
+                                if (ButtonStatistics.isPrimaryHovered)
+                                {
+                                    cMenu.DisplaySetOperation(CurrentMenu.SetOperation.SetRelativeComplement);
+                                    break;
+                                }
+
+                                if (ButtonHide_Show.isPrimaryHovered)
+                                {
+                                    cMenu.DisplaySetOperation(CurrentMenu.SetOperation.Return);
+                                    break;
+                                }
+
+                                cMenu.DisplaySetOperation(cMenu.GetCurrentMenuSetOperation());
+                                break;
+                            }
+
+                        default: //Selection Menu behavior
+                            {
+                                if (ButtonCreation.isPrimaryHovered)
+                                {
+                                    cMenu.DisplaySelection(CurrentMenu.Selection.Modification);
+                                    break;
+                                }
+
+                                if (ButtonSelection.isPrimaryHovered)
+                                {
+                                    cMenu.DisplaySelection(CurrentMenu.Selection.Erase);
+                                    break;
+                                }
+
+
+                                if (ButtonStatistics.isPrimaryHovered)
+                                {
+                                    cMenu.DisplaySelection(CurrentMenu.Selection.SetOperation);
+                                    break;
+                                }
+
+                                if (ButtonHide_Show.isPrimaryHovered)
+                                {
+                                    cMenu.DisplaySelection(CurrentMenu.Selection.Return);
+                                    break;
+                                }
+
+                                cMenu.DisplaySelection(cMenu.GetCurrentMenuSelection());
+                                break;
+                            }
+                    }
+
+                    break;
+
+                }
+
+            default: // General Menu Behavior
+                {
+                    if (ButtonCreation.isPrimaryHovered)
+                    {
+                        cMenu.DisplayMenu(CurrentMenu.Menu.Creation);
+                        break;
+                    }
+                    
+                    if (ButtonSelection.isPrimaryHovered)
+                    {
+                        cMenu.DisplayMenu(CurrentMenu.Menu.Selection);
+                        break;
+                    }
+
+                    if (ButtonStatistics.isPrimaryHovered)
+                    {
+                        cMenu.DisplayMenu(CurrentMenu.Menu.Statistics);
+                        break;
+                    }
+
+                    if (ButtonHide_Show.isPrimaryHovered)
+                    {
+                        cMenu.DisplayMenu(CurrentMenu.Menu.Hide_Show);
+                        break;
+                    }
+
+                    if (ButtonHelp_Options.isPrimaryHovered)
+                    {
+                        cMenu.DisplayMenu(CurrentMenu.Menu.Help_Options);
+                        break;
+                    }
+
+                    if (ButtonQuit.isPrimaryHovered)
+                    {
+                        cMenu.DisplayMenu(CurrentMenu.Menu.Quit);
+                        break;
+                    }
+
+                    cMenu.DisplayMenu(cMenu.GetCurrentMenu());
+                    break;
+                }
+
+        }
+
+        return;
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -136,8 +262,11 @@ public class MenuInteraction : MonoBehaviour
     void Update()
     {
         
+        /*
+            Important notice : when changing pages, the menu waits 120 frames before allowing the user to change current mode or page.
+         */
 
-        if(ChangeMenuPage && FrameCounter < 120)
+        if(ChangeMenuPage && FrameCounter < 120) // 120 should be defined according to system specs.
         {
             FrameCounter++;
         }
@@ -146,5 +275,8 @@ public class MenuInteraction : MonoBehaviour
             FrameCounter = 0;
             ChangeMenuPage = MenuManager();
         }
+
+        MenuPreview();
+
     }
 }
