4da86ac5c579390fcf9a704c3c8eda3ac0a4e01c
Toastation <toastations@gmail.com>
2020-02-19 22:04:06 +0100

    changed the rendering of the data points

--- Assets/Scripts/ScatterPlot.cs
+++ Assets/Scripts/ScatterPlot.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(ParticleSystem))]
 public class ScatterPlot : MonoBehaviour
 {
 
@@ -10,22 +10,27 @@ public class ScatterPlot : MonoBehaviour
 
     public GameObject PointPrefab;
 
-    /** The list of points in the scatterplot */
+    /** The list of points in the scatterplot and their particle representation */
+    private ParticleSystem pSystem;
     private List<DataPoint> dataPoints;
+    private ParticleSystem.Particle[] dataParticles;
 
     void Start()
     {
         // load the points from the csv and prints the number of points loaded
+        var watch = System.Diagnostics.Stopwatch.StartNew();
         dataPoints = LoadCSV.LoadCSVFile(csvPath);
-        Debug.Log("Loaded " + dataPoints.Count + " points");
+        watch.Stop();
+        var elapsedMs = watch.ElapsedMilliseconds;
+        Debug.Log("Loaded " + dataPoints.Count + " points in "+elapsedMs+" ms");
 
         //instantiate prefab
-        foreach (DataPoint dp in dataPoints)
-        {
-            GameObject dpInstance = Instantiate(PointPrefab, dp.GetPos(), Quaternion.identity);
-            dpInstance.GetComponent<Renderer>().material.color = dp.GetColor();
-            dp.SetGameObject(dpInstance);
-        }
+        pSystem = GetComponent<ParticleSystem>();
+        watch = System.Diagnostics.Stopwatch.StartNew();
+        InitParticles();
+        watch.Stop();
+        elapsedMs = watch.ElapsedMilliseconds;
+        Debug.Log("Instanciated point prefabs in " + elapsedMs + " ms");
     }
 
     void Update()
@@ -39,6 +44,28 @@ public class ScatterPlot : MonoBehaviour
                 Debug.Log(volumes[0].Contains(dp.GetPos()));
             }
         }
+    }
+
+    private void InitDataPoints()
+    {
+        foreach (DataPoint dp in dataPoints)
+        {
+            GameObject dpInstance = Instantiate(PointPrefab, dp.GetPos(), Quaternion.identity);
+            dpInstance.GetComponent<Renderer>().material.color = dp.GetColor();
+            dp.SetGameObject(dpInstance);
+        }
+    }
+
+    private void InitParticles()
+    {
+        dataParticles = new ParticleSystem.Particle[dataPoints.Count];
+        for (int i = 0; i < dataPoints.Count; i++)
+        {
+            dataParticles[i].position = dataPoints[i].GetPos();
+            dataParticles[i].startColor = dataPoints[i].GetColor();
+            dataParticles[i].startSize = 0.01f;
+        }
+        pSystem.SetParticles(dataParticles, dataParticles.Length);
     }
 
     /**
@@ -77,6 +104,5 @@ public class ScatterPlot : MonoBehaviour
         }
         return pointsInVolume;
     }
-    
 
 }
