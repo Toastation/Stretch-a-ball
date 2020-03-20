9cef107b05dffa6601441f2e9e068fe584d3097e
Lucas T <lucas.trocherie72@gmail.com>
2020-03-20 10:30:29 -0700

    Created a dynamic canvas with text for the stats : just needs to be filled !

---
+++ Assets/Scripts/Statistics.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Statistics : MonoBehaviour
+{
+    private Canvas StatisticsCanvas; // Assign in inspector
+    CurrentMenu cMenu;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StatisticsCanvas = GetComponent<Canvas>();
+        cMenu = GameObject.Find("Palm UI L").GetComponent<CurrentMenu>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cMenu.GetCurrentMenu() == CurrentMenu.Menu.Statistics)
+        {
+            if(!StatisticsCanvas.enabled)
+                StatisticsCanvas.enabled = !StatisticsCanvas.enabled;
+        }
+        else
+        {
+            if (StatisticsCanvas.enabled)
+                StatisticsCanvas.enabled = !StatisticsCanvas.enabled;
+        }
+    }
+}
