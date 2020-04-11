8508ec94bbb4da4cc4f41bb4109839b97eeec1e7
Lucas T <lucas.trocherie72@gmail.com>
2020-04-10 18:21:58 -0700

    Added display for the camera position

---
+++ Assets/PositionDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.IO;
+public class PositionDisplay : MonoBehaviour
+{
+    private Canvas PositionCanvas; // Assign in inspector
+    Text positionDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PositionCanvas = GetComponent<Canvas>();
+        positionDisplay = GameObject.Find("Position").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        positionDisplay.text = "Position" + Camera.main.transform.position;
+    }
+}
